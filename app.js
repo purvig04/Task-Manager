8fa552f9fc4892c170a044b11fd286c704b63e5a
purvi <purvig@id.argusoft.com>
2025-08-07 14:31:08 +0530

    initialize the project

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+  res.end();
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
