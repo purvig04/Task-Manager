d5c2fd7177c6d82b70581e86c60c6b6b415e5fa7
purvi <purvig@id.argusoft.com>
2025-08-11 16:03:11 +0530

    modified app.js

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
+app.use(express.static("./public"));
 app.use(express.json());
-
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-  res.end();
-});
 
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
