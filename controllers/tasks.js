7b2665deb2b7aebfafd4fd698bad3f240f167b20
purvi <purvig@id.argusoft.com>
2025-08-11 14:36:02 +0530

    modified controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,16 @@
+const Task = require("../models/tasks");
+
 const getAllTasks = (req, res) => {
   res.send(" get All tasks");
 };
 
-const createTask = (req, res) => {
-  res.send("create Task");
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
 };
 
 const getTask = (req, res) => {
-  res.send("get single Task");
+  res.json({ id: req.params.id });
 };
 
 const updateTask = (req, res) => {
