cb62408836f38166b49724f94bbcf8f0cf7cc374
purvi <purvig@id.argusoft.com>
2025-08-11 09:49:58 +0530

    add controllers and routes

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
+
+app.use(express.json());
 
 app.get("/hello", (req, res) => {
   res.send("Task Manager App");
   res.end();
 });
+
+app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
