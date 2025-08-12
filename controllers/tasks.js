0b6cd5a1264dc9d500837ab08c98c49ee9355322
purvi <purvig@id.argusoft.com>
2025-08-12 11:01:36 +0530

    error handling

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,66 +1,53 @@
 const Task = require("../models/tasks");
+const asyncWrapper = require("../middleware/async");
+const { createCustomError } = require("../errors/custom-error");
 
-const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).json({ tasks }); //shorthand property
-  } catch (error) {
-    res.status(500).json({ msg: error });
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks }); //shorthand property
+  //res.status(200).json({status:"success", data : {tasks , amount : tasks.length}})//here task array will be stored in data.
+});
+
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
+
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOne({ _id: taskID });
+
+  if (!task) {
+    return next(createCustomError(`No task with id ${taskID}`, 404));
   }
-};
+  res.status(200).json({ task });
+});
 
-const createTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+const updateTask = asyncWrapper(async (req, res, next) => {
+  const { id: TaskID } = req.params;
+  const task = await Task.findOneAndUpdate({ _id: TaskID }, req.body, {
+    new: true,
+    runValidators: true,
+  }); //these options will return the updated values only (otherwise it was just returning the old value while updated the old one) and also validators of schema wll also be applied on the updated value.
+  if (!task) {
+    return next(createCustomError(`No task with id ${TaskID}`, 404));
+    // const error = new Error("not found");
+    // error.status = 404;
+    // return next(error);
+    // // return res.status(404).json({ msg: `No task with id : ${TaskID}` });
   }
-};
 
-const getTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOne({ _id: taskID });
+  res.status(200).json({ task });
+});
 
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id ${taskID}` }); //correct syntax for the id but not registered
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error }); //syntax or any other error
+const deleteTask = asyncWrapper(async (req, res, next) => {
+  const { id: TaskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: TaskID });
+  if (!task) {
+    return next(createCustomError(`No task with id ${taskID}`, 404));
   }
-};
-
-const updateTask = async (req, res) => {
-  try {
-    const { id: TaskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: TaskID }, req.body, {
-      new: true,
-      runValidators: true,
-    }); //these options will return the updated values only (otherwise it was just returning the old value while updated the old one) and also validators of schema wll also be applied on the updated value.
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id : ${TaskID}` });
-    }
-
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
-
-const deleteTask = async (req, res) => {
-  try {
-    const { id: TaskID } = req.params;
-    const task = await Task.findOneAndDelete({ _id: TaskID });
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id : ${TaskID}` });
-    }
-    res.status(200).json({ task }); // just for the postman to see what it deleted
-    // res.status(200).send() //becoz we dont generally see in frontend what has been deleted
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+  res.status(200).json({ task }); // just for the postman to see what it deleted
+  // res.status(200).send() //becoz we dont generally see in frontend what has been deleted
+});
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
